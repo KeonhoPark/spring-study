f70b28ba32fcb0d28615ecb704f6a93a56671868
KeonhoPark <gse04063@gmail.com>
2022-05-31 20:22:19 +0900

    스프링으로 전환하기(스프링 프레임워크를 이용한 DI)

--- hello-spring/core/src/main/java/hello/core/OrderApp.java
+++ hello-spring/core/src/main/java/hello/core/OrderApp.java
@@ -4,19 +4,25 @@ import hello.core.member.*;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//        OrderService orderService = appConfig.orderService();
+//        MemberService memberService = appConfig.memberService();
 
-        OrderService orderService = appConfig.orderService();
-        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "member1", Grade.VIP);
         memberService.join(member);
-        Order order = orderService.createOrder(memberId, "item1", 10000);
+        Order order = orderService.createOrder(memberId, "item1", 20000);
 
         System.out.println("order = " + order);
         System.out.println("order.calculatePrice() = " + order.calculatePrice());
