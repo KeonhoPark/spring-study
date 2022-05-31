3058c1d14d205301fe59b5c3ccb6e6f6b07464c0
KeonhoPark <gse04063@gmail.com>
2022-05-31 16:39:01 +0900

    AppConfig 생성을 통한 관심사 분리, 생성자를 통한 의존성 주입(자바)

--- hello-spring/core/src/main/java/hello/core/OrderApp.java
+++ hello-spring/core/src/main/java/hello/core/OrderApp.java
@@ -8,8 +8,10 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        OrderService orderService = new OrderServiceImpl();
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+
+        OrderService orderService = appConfig.orderService();
+        MemberService memberService = appConfig.memberService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "member1", Grade.VIP);
