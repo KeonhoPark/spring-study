cd0f5d4910bde971d81dda6b72ecbf44f4d6d39e
KeonhoPark <gse04063@gmail.com>
2022-06-02 15:37:12 +0900

    @Configuration과 싱글톤, 스프링 컨테이너의 싱글톤 보장

---
+++ hello-spring/core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,31 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest(){
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        System.out.println("orderService repository = " + orderService.getMemberRepository());
+        System.out.println("memberService repository = " + memberService.getMemberRepository());
+        System.out.println("repository = " + memberRepository);
+
+        Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+
+    }
+}
