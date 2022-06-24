b5b0efa42f79490cce1bd6333bc1a83267b54d66
KeonhoPark <gse04063@gmail.com>
2022-06-24 15:02:46 +0900

    HTTP 요청 파라미터 - 쿼리 파라미터, HTML Form (HttpServletRequest의 getParameter()를 사용한 Post 방식의 HTML Form 요청 파라미터 가져오기)

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,27 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response,
+                               @RequestHeader MultiValueMap<String, String> headerMap) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username = {}, age = {}", username, age);
+        log.info("headerMap = {}", headerMap);
+
+        response.getWriter().write("OK");
+    }
+}
