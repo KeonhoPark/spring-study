656d7b13bd307b2cfa751077ac8ae8da1414a051
KeonhoPark <gse04063@gmail.com>
2022-06-27 16:46:12 +0900

    상품 도메인 개발

---
+++ item-service/src/test/java/hello/itemService/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,72 @@
+package hello.itemService.domain.item;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach(){
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save(){
+
+        //given
+        Item item = new Item("ItemA", 10000, 100);
+
+        //when
+        Item savedItem = itemRepository.save(item);
+
+        //then
+        Item findItem = itemRepository.findById(savedItem.getId());
+        assertThat(savedItem).isEqualTo(findItem);
+    }
+
+    @Test
+    void findAll(){
+
+        //given
+        Item item1 = new Item("ItemA", 10000, 100);
+        Item item2 = new Item("ItemB", 1000, 10);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+        //when
+
+        List<Item> result = itemRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1, item2);
+    }
+
+    @Test
+    void updateItem(){
+
+        //given
+        Item item = new Item("ItemA", 10000, 100);
+
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
+
+        //when
+        Item updateParam = new Item("ItemB", 20000, 30);
+        itemRepository.update(itemId, updateParam);
+
+        //then
+        Item findItem = itemRepository.findById(itemId);
+
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+    }
+}
