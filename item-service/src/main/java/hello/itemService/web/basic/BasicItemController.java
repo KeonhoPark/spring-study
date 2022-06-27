25c250669ce154b8082e35eb573b613a15a70c86
KeonhoPark <gse04063@gmail.com>
2022-06-27 21:58:46 +0900

    상품 등록 페이지에서 새로고침 할때 문제점 개선(Post/Redirect/Get)

--- item-service/src/main/java/hello/itemService/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemService/web/basic/BasicItemController.java
@@ -62,12 +62,21 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item){
 
         itemRepository.save(item);
         return "basic/item";
     }
+
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item){
+
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+
 
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model){
