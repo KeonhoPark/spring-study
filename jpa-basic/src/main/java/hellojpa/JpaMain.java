cb28cb5580880d26202e88007f2d13cd296f6e69
KeonhoPark <81609381+KeonhoPark@users.noreply.github.com>
2022-07-08 12:45:20 +0900

    Hello JPa 애플리케이션 개발

---
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,39 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setName("park");
+            member.setId(2L);
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, 1L);
+            List<Member> resultList = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
