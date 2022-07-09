a8ce8a4d698d87750eec03afaea42745d7152846
KeonhoPark <81609381+KeonhoPark@users.noreply.github.com>
2022-07-09 22:16:25 +0900

    기본 키 매핑

---
+++ jpa-basic/src/main/java/hellojpa/Member.java
@@ -0,0 +1,36 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+@SequenceGenerator(name = "member_seq_generator",
+                   sequenceName = "MEMBER_SEQ")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+                    generator = "member_seq_generator")
+    private Long id;
+
+    @Column(name = "name")
+    private String username;
+
+    public Member() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
