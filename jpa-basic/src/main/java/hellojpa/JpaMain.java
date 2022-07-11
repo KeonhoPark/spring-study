18a59a23ac46f1d6c42950e8ffc8d5971f1b70bf
KeonhoPark <81609381+KeonhoPark@users.noreply.github.com>
2022-07-11 22:18:24 +0900

    프록시, 즉시 로딩과 지연 로딩

--- jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,12 @@
 package hellojpa;
 
+import hellojpa.items.Movie;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -19,19 +22,32 @@ public class JpaMain {
         try {
 
             Team team = new Team();
-            team.setName("TeamA");
+            team.setName("teamA");
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("memberA");
-            member.setTeam(team);
+            member.setUsername("member1");
+            member.setCreatedDate(LocalDateTime.now());
+            member.setCreatedBy("admin");
+            member.changeTeam(team);
             em.persist(member);
 
-            Team findTeam = member.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+
+            System.out.println("findMember = " + findMember.getTeam().getClass());
+
+            System.out.println("================");
+            findMember.getTeam().getName();
+
+            System.out.println("================");
 
             tx.commit();
         } catch (Exception e) {
+            e.printStackTrace();
             tx.rollback();
         } finally {
             em.close();
