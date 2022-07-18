124e3c6c6b361cdb5cbdca2fe4a2892a3da30a3c
KeonhoPark <81609381+KeonhoPark@users.noreply.github.com>
2022-07-18 22:00:39 +0900

    주문 조회 V3 : 페치 조인 최적화

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -108,10 +108,20 @@ public class OrderRepository {
 
         List<OrderSimpleQueryDto> result = em.createQuery("select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
                         "from Order o join o.member m join o.delivery d", OrderSimpleQueryDto.class)
-                        .getResultList();
+                .getResultList();
 
         return result;
 
 
+    }
+
+    public List<Order> findAllWithItem() {
+
+        return em.createQuery("select distinct o from Order o " +
+                        "join fetch o.member m " +
+                        "join fetch o.delivery d " +
+                        "join fetch o.orderItems oi " +
+                        "join fetch oi.item", Order.class)
+                            .getResultList();
     }
 }
