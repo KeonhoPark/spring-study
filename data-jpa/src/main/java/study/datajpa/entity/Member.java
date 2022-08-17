c8e69781941d484f20f748a741d3ad4f43db13d6
KeonhoPark <gse04063@gmail.com>
2022-08-18 00:15:49 +0900

    Auditing

--- data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -6,13 +6,14 @@ import javax.persistence.*;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age"})
 @NamedQuery(
         name = "Member.findByUsername",
         query = "select m from Member m where m.username = :username"
 )
-public class Member {
+public class Member extends BaseEntity{
 
     @Id
     @GeneratedValue
