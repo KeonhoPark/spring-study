f9632d8e96e6c886865fe0540e3eeaa40147c9ad
KeonhoPark <gse04063@gmail.com>
2022-08-19 15:05:49 +0900

    검색 조건 쿼리

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -72,4 +72,15 @@ public class QuerydslBasicTest {
         assertThat(result.getUsername()).isEqualTo("member1");
     }
 
+    @Test
+    public void search() {
+        Member findMember = query
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        ,member.age.eq(10))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
+
 }
